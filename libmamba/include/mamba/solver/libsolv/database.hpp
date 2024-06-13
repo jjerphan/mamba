39a2679727e90ec59bb7428d7b1495e9d7f4d72a
Julien Jerphanion <git@jjerphan.xyz>
2024-06-13 17:05:40 +0200

    wip
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- libmamba/include/mamba/solver/libsolv/database.hpp
+++ libmamba/include/mamba/solver/libsolv/database.hpp
@@ -142,8 +142,6 @@ namespace mamba::solver::libsolv
             friend class UnSolvable;
         };
 
-    private:
-
         struct DatabaseImpl;
 
         std::unique_ptr<DatabaseImpl> m_data;
