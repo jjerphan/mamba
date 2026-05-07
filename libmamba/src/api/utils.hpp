0ced4f4d6557f90cc9e353c86c79e122ea125f71
Julien Jerphanion <git@jjerphan.xyz>
2026-05-07 15:05:23 +0200

    Solver switch
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- libmamba/src/api/utils.hpp
+++ libmamba/src/api/utils.hpp
@@ -118,7 +118,7 @@ namespace mamba
         bool experimental_matchspec_parsing,
         solver::libsolv::Database& db,
         const solver::Request& request,
-        std::string_view solver_name = "libsolv"
+        bool use_resolvo = false
     );
 
     /**
